becc8e24b76a1bdc7da2cfe177e6596abb8a3f9b
Archi- <waseemjr@gmail.com>
2016-01-21 11:41:49 +0100

    Added run method, can now run main method and launch the app(Screen).

--- src/LOGIC/Controller.java
+++ src/LOGIC/Controller.java
@@ -1,7 +1,26 @@
 package LOGIC;
 
+import GUI.*;
+import SDK.*;
+
 /**
  * Created by Waseem on 19/01/16.
  */
 public class Controller {
+
+    private Screen screen;
+
+
+
+    public Controller() {
+        screen = new Screen();
+        screen.setVisible(true);
+
+
+    }
+
+    public void run() {
+
+    }
+
 }
--- src/RUN/Main.java
+++ src/RUN/Main.java
@@ -1,7 +1,13 @@
 package RUN;
 
+import LOGIC.Controller;
+
 /**
  * Created by Waseem on 19/01/16.
  */
 public class Main {
+    public static void main(String[] args) {
+            Controller controller = new Controller();
+            controller.run();
+    }
 }
